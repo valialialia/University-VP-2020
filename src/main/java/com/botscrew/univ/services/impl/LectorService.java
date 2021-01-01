e92f10ca2ad5d0e54b972a58f985cb8b8eb27fef
Valentyna Pivnytska <pivnytska@gmail.com>
2021-01-01 13:44:05 +0200

    Created LectorService

---
+++ src/main/java/com/botscrew/univ/services/impl/LectorService.java
@@ -0,0 +1,14 @@
+package com.botscrew.univ.services.impl;
+
+
+import com.botscrew.univ.models.impl.Lector;
+import com.botscrew.univ.services.ILectorService;
+
+public class LectorService implements ILectorService {
+
+    @Override
+    public Lector getLectorLike(String nameLike) {
+
+        return null;
+    }
+}
